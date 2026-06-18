c081983c02f722257b6d55b8c004dd40d664b523
Nikita Jegorov <proxjega@gmail.com>
2026-06-18 15:25:23 +0300

    init

---
+++ Models/User.cs
@@ -0,0 +1,16 @@
+// namespace AspNetProject.Models;
+
+// public class User
+// { 
+//     public long Id { get; set; }
+
+//     [Required]
+//     public string Name { get; set; }
+//     public string Surname { get; set; }
+//     public int DateOfBirth { get; set; }
+//     public string Gender { get; set; }
+//     public string Email { get; set; }
+//     public string Address { get; set; }
+//     public string Email { get; set; }
+    
+// }
