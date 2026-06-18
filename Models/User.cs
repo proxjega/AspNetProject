cf5829ad76af9694373c89c20e73fa16421092ff
Nikita Jegorov <proxjega@gmail.com>
2026-06-18 16:55:35 +0300

    user model and UserContext

--- Models/User.cs
+++ Models/User.cs
@@ -1,16 +1,30 @@
-// namespace AspNetProject.Models;
+using System.ComponentModel.DataAnnotations;
 
-// public class User
-// { 
-//     public long Id { get; set; }
+namespace AspNetProject.Models;
 
-//     [Required]
-//     public string Name { get; set; }
-//     public string Surname { get; set; }
-//     public int DateOfBirth { get; set; }
-//     public string Gender { get; set; }
-//     public string Email { get; set; }
-//     public string Address { get; set; }
-//     public string Email { get; set; }
+public class User
+{ 
+    public long Id { get; set; }
+
+    [Required]
+    [StringLength(30)]
+    public string? Name { get; set; }
+
+    [Required]
+    [StringLength(30)]
+    public string? Surname { get; set; }
+
+    [Range(1900, 9999)]
+    public int DateOfBirth { get; set; }
+
+    public string? Gender { get; set; }
+
+    [EmailAddress]
+    public string? Email { get; set; }
+
+    [StringLength(100)]
+    public string? Address { get; set; }
+
+    public List<string> UserNotes { get; set; } = new();
     
-// }
+}
--- Program.cs
+++ Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using AspNetProject.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,6 +8,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddDbContext<UserContext>(opt =>
+    opt.UseInMemoryDatabase("Users"));
 
 var app = builder.Build();
 
