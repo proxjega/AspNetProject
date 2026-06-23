e23d52acfb0f30c4a68a75710a50b8c9ac975e02
Nikita Jegorov <proxjega@gmail.com>
2026-06-23 15:30:06 +0300

    dto part 1 + updated put

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -22,14 +22,16 @@ namespace AspNetProject.Controllers
 
         // GET: api/User
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<User>>> GetUsers()
+        public async Task<ActionResult<IEnumerable<UserDTO>>> GetUsers()
         {
-            return await _context.Users.ToListAsync();
+            return await _context.Users
+        .Select(x => UserToDTO(x))
+        .ToListAsync();
         }
 
         // GET: api/User/5
         [HttpGet("{id}")]
-        public async Task<ActionResult<User>> GetUser([FromRoute] long id)
+        public async Task<ActionResult<UserDTO>> GetUser([FromRoute] long id)
         {
             var user = await _context.Users.FindAsync(id);
 
@@ -38,7 +40,7 @@ namespace AspNetProject.Controllers
                 return NotFound();
             }
 
-            return user;
+            return UserToDTO(user);
         }
 
         // PUT: api/User/5
@@ -51,7 +53,15 @@ namespace AspNetProject.Controllers
                 return BadRequest();
             }
 
-            _context.Entry(user).State = EntityState.Modified;
+            var currentUser = await _context.Users.FindAsync(id);
+            if(currentUser == null) {
+                return NotFound();
+            }
+
+            currentUser.Name = user.Name;
+            currentUser.Surname = user.Surname;
+            currentUser.Gender = user.Gender;
+            currentUser.Email = user.Email;
 
             try
             {
@@ -103,5 +113,15 @@ namespace AspNetProject.Controllers
         {
             return _context.Users.Any(e => e.Id == id);
         }
+
+        private static UserDTO UserToDTO(User user) =>
+            new UserDTO
+            {
+                Id = user.Id,
+                Name = user.Name,
+                Surname = user.Surname,
+                Email = user.Email,
+                Gender = user.Gender
+            };
     }
 }
--- Models/UserDTO.cs
+++ Models/UserDTO.cs
@@ -2,14 +2,7 @@ using System.ComponentModel.DataAnnotations;
 
 namespace AspNetProject.Models;
 
-public enum GenderType
-{
-    Male,
-    Female,
-    NonBinary,
-}
-
-public class User
+public class UserDTO
 { 
     public long Id { get; set; }
 
@@ -21,18 +14,10 @@ public class User
     [StringLength(30)]
     public string? Surname { get; set; }
 
-    [Range(1900, 9999)]
-    public int DateOfBirth { get; set; }
-
     public GenderType? Gender { get; set; }
 
     [EmailAddress]
     public string? Email { get; set; }
-
-    [StringLength(100)]
-    public string? Address { get; set; }
-
-    public List<string> UserNotes { get; set; } = new();
 
     
 }
