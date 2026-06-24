e50005d65db5240ed7a77f218e5e9030365853fc
Nikita Jegorov <proxjega@gmail.com>
2026-06-24 15:55:16 +0300

    gitignore

--- Program.cs
+++ Program.cs
@@ -8,7 +8,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<UserContext>(opt =>
-    opt.UseInMemoryDatabase("Users"));
+    opt.UseSqlServer());
 
 var app = builder.Build();
 
